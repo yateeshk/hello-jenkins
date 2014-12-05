a38654830f0976ec65309e7ec7898aa2b18dab17
yateeshk <yateeshk@gmail.com>
2014-12-05 07:17:45 -0500

    Add node app

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+ 
+var app = express();
+var sys = require('sys')
+
+console.log("hello");
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
