36464b7915af08ae7bafc0e2e343085db71ed7bd
root <root@sys-69904.dal-ebis.ihost.com>
2014-12-05 08:42:25 -0500

    hello x

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var sys = require('sys')
 
 console.log("hello");
 app.get('/', function (req, res) {
-  res.send('hello srirama');
+  res.send('hello u ');
 });
  
 app.listen(process.env.PORT || 5000);
